e0775540069b0653564b027367197c04a935c270
Anshul Sharma <optimisticanshul@gmail.com>
2015-11-29 20:06:32 +0530

    Modified For Opsworks

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,6 +11,10 @@ default['zookeeper-cluster']['config']['properties']['initLimit'] = 5
 default['zookeeper-cluster']['config']['properties']['syncLimit'] = 2
 default['zookeeper-cluster']['config']['properties']['leaderServes'] = 'yes'
 default['zookeeper-cluster']['config']['properties']['forceSync'] = 'no'
+default['zookeeper-cluster']['config']['instance_name'] = node['ipaddress']
+nodes = []
+search(:node, "name:*").each { |node| nodes << node.attributes.private_ip }
+default['zookeeper-cluster']['config']['ensemble'] = nodes
 
 default['zookeeper-cluster']['service_name'] = 'zookeeper'
 default['zookeeper-cluster']['service_user'] = 'zookeeper'
@@ -20,3 +24,4 @@ default['zookeeper-cluster']['service']['environment']['JMXPORT'] = 9010
 default['zookeeper-cluster']['service']['version'] = '3.5.0-alpha'
 default['zookeeper-cluster']['service']['binary_checksum'] = '87814f3afa9cf846db8d7e695e82e11480f7b19d79d8f146e58c4aefb4289bf4'
 default['zookeeper-cluster']['service']['binary_url'] = "http://mirror.cc.columbia.edu/pub/software/apache/zookeeper/zookeeper-%{version}/zookeeper-%{version}.tar.gz" # rubocop:disable Style/StringLiterals
+
